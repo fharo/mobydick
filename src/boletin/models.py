cc5e9b655cb969dd6e70a2626bd42bf23e4ccf0c
Francisco Haro <fharo@outlook.com>
2020-04-14 10:07:52 -0500

    Correccion en la migración de boletin

--- src/boletin/models.py
+++ src/boletin/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Registro(models.Model):
+    nombres = models.CharField(max_length=100)
+    apellidos = models.CharField(max_length=100)
+    correo = models.EmailField()
+    telefono = models.CharField(max_length=7)
+    celular = models.CharField(max_length=10)
+    timeslamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+    def __unicode__(self):
+        return self.email
+
+    def __str__(self):
+        return self.email
